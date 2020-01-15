e5c1e208c52c18b5fadd038a4f0ab4251bd8d2c6
Vadim Kholoshnya <vhd-ru@yandex.ru>
2020-01-15 18:14:09 +0300

    Update files
    Изменено:
    Vacancies -> Vacancies.py
    description -> description.csv
    tqdm_notebook -> tqdm
    
    Добавлено:
    Функция "main"
    Описание для функций "tqdm"

--- Vacancies.py
+++ Vacancies.py
@@ -1,40 +1,45 @@
-import numpy as np
-import requests
-from tqdm import tqdm_notebook
-import pandas as pd
 import re
+import requests
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
 
-'''
-Список специализаций
-spec = []
-spec.append(requests.get("https://api.hh.ru/specializations").json())
+def main():
 
-Задача: выгруженный список отфильтровать от специальностей, не связанных со специализацией Программное и аппаратное обеспечение встраиваемых систем
-'''
+    '''
+    Список специализаций
+    spec = []
+    spec.append(requests.get("https://api.hh.ru/specializations").json())
 
-'''
-Для специализации с Id=1.274
-'''
-vac = []
-for i in tqdm_notebook(range(0, 98)):
-    vac.append(requests.get("https://api.hh.ru/vacancies?specialization=1.274", params={'page': i, 'per_page':20}).json())
-    
-pac=[]
-for i in range(0, 39):
-    for j in range(0, 20):
-        pac.append(vac[i]['items'][j]['alternate_url'])
+    Задача: выгруженный список отфильтровать от специальностей, не связанных со специализацией Программное и аппаратное обеспечение встраиваемых систем
+    '''
 
-lili = [re.sub(r'[^0-9]', '', e) for e in pac]
-vak_url = 'https://api.hh.ru/vacancies/{}'
- 
-var = []
-for i in lili:
-    var.append(requests.get(vak_url.format(i)).json())
+    '''
+    Для специализации с Id=1.274
+    '''
 
-df = pd.DataFrame(var)
+    vac = []
+    for i in tqdm(range(0, 98), 'Формирование массива vac'):
+        vac.append(requests.get('https://api.hh.ru/vacancies?specialization=1.274', params={'page': i, 'per_page':20}).json())
 
-df['description'] = df['description'].apply(lambda x: (re.sub(r'<.*?>', '', str(x))))
+    pac = []
+    for i in range(0, 39): # tqdm(range(0, 39), 'Формирование массива pac'):
+        for j in range(0, 20):
+            pac.append(vac[i]['items'][j]['alternate_url'])
 
-file_name = 'description'
-df.to_csv(file_name, columns =['description'])
+    lili = [re.sub(r'[^0-9]', '', e) for e in pac] # tqdm(pac, 'Формирование массива lili')
+    vak_url = 'https://api.hh.ru/vacancies/{}'
 
+    var = []
+    for i in tqdm(lili, 'Формирование массива var'):
+        var.append(requests.get(vak_url.format(i)).json())
+
+    df = pd.DataFrame(var)
+
+    df['description'] = df['description'].apply(lambda x: (re.sub(r'<.*?>', '', str(x))))
+
+    file_name = 'description.csv'
+    df.to_csv(file_name, columns =['description'])
+
+if __name__ == "__main__":
+    main()
